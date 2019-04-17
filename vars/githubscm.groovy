5a7268c7a07be1d42b21b33bd05a5f0fc4695d52
tzimanyi <tibor.zimanyi@gmail.com>
2019-04-17 12:23:29 +0200

    Extract Maven methods to a separate variable

--- vars/githubscm.groovy
+++ vars/githubscm.groovy
@@ -25,17 +25,3 @@ def checkoutIfExists(String repository, String author, String branches, String d
         checkout(resolveRepository(repository, defaultAuthor, defaultBranches, false))
     }
 }
-
-def runMavenWithSettings(String settingsXmlId, String goals, boolean skipTests) {
-    configFileProvider([configFile(fileId: settingsXmlId, variable: 'MAVEN_SETTINGS_XML')]) {
-        def mvnCommand = 'mvn -s $MAVEN_SETTINGS_XML -fae ' + goals
-        if (skipTests) {
-            mvnCommand = mvnCommand + ' -DskipTests'
-        }
-        sh mvnCommand
-    }
-}
-
-def runMavenWithSubmarineSettins(String goals, boolean skipTests) {
-    runMavenWithSettings('9239af2e-46e3-4ba3-8dd6-1a814fc8a56d', goals, skipTests)
-}
---
+++ vars/maven.groovy
@@ -0,0 +1,13 @@
+def runMavenWithSettings(String settingsXmlId, String goals, boolean skipTests) {
+    configFileProvider([configFile(fileId: settingsXmlId, variable: 'MAVEN_SETTINGS_XML')]) {
+        def mvnCommand = 'mvn -s $MAVEN_SETTINGS_XML -fae ' + goals
+        if (skipTests) {
+            mvnCommand = mvnCommand + ' -DskipTests'
+        }
+        sh mvnCommand
+    }
+}
+
+def runMavenWithSubmarineSettins(String goals, boolean skipTests) {
+    runMavenWithSettings('9239af2e-46e3-4ba3-8dd6-1a814fc8a56d', goals, skipTests)
+}
