1c88dfa8abaee65cc2632cd84eee7cda5a7740b9
tzimanyi <tibor.zimanyi@gmail.com>
2019-04-17 12:14:21 +0200

    Add methods to run Maven with custom settings.xml

--- vars/githubscm.groovy
+++ vars/githubscm.groovy
@@ -25,3 +25,17 @@ def checkoutIfExists(String repository, String author, String branches, String d
         checkout(resolveRepository(repository, defaultAuthor, defaultBranches, false))
     }
 }
+
+def runMavenWithSettings(String settingsXmlId, String goals, boolean skipTests) {
+    configFileProvider([configFile(fileId: '9239af2e-46e3-4ba3-8dd6-1a814fc8a56d', variable: 'MAVEN_SETTINGS_XML')]) {
+        def mvnCommand = 'mvn -s $MAVEN_SETTINGS_XML -fae ' + goals
+        if (skipTests) {
+            mvnCommand = mvnCommand + ' -DskipTests'
+        }
+        sh mvnCommand
+    }
+}
+
+def runMavenWithSubmarineSettins(String goals, boolean skipTests) {
+    runMavenWithSettings('9239af2e-46e3-4ba3-8dd6-1a814fc8a56d', goals, skipTests)
+}
