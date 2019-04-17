57cf8cd5c2faf29367099c6d5a4ae6ac3a74e95f
tzimanyi <tibor.zimanyi@gmail.com>
2019-04-17 13:35:41 +0200

    Use user kie-ci1-token in PR automation

--- vars/githubscm.groovy
+++ vars/githubscm.groovy
@@ -1,7 +1,7 @@
 def resolveRepository(String repository, String author, String branches, boolean ignoreErrors) {
     return resolveScm(
             source: github(
-                    credentialsId: 'kie-ci2-token',
+                    credentialsId: 'kie-ci1-token',
                     repoOwner: author,
                     repository: repository,
                     traits: [[$class: 'org.jenkinsci.plugins.github_branch_source.BranchDiscoveryTrait', strategyId: 1],
