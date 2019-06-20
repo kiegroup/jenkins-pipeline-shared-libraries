b787a94526b0101363ae6cd90f2855609f127321
Radovan Synek <rsynek@redhat.com>
2019-06-20 11:07:06 +0200

    BAQE-975 Pass properties to a maven build

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,13 +1,31 @@
-def runMavenWithSettings(String settingsXmlId, String goals, boolean skipTests) {
+import java.util.Properties
+
+final def SUBMARINE_SETTINGS = '9239af2e-46e3-4ba3-8dd6-1a814fc8a56d'
+
+def runMavenWithSettings(String settingsXmlId, String goals, Properties properties) {
     configFileProvider([configFile(fileId: settingsXmlId, variable: 'MAVEN_SETTINGS_XML')]) {
-        def mvnCommand = 'mvn -B -s $MAVEN_SETTINGS_XML -fae ' + goals
-        if (skipTests) {
-            mvnCommand = mvnCommand + ' -DskipTests'
+        def propertiesString = ''
+
+        properties.each { key, value ->
+            propertiesString += "-D$key=$value "
         }
+
+        def mvnCommand = 'mvn -B -s $MAVEN_SETTINGS_XML -fae ' + goals + " $propertiesString"
+
         sh mvnCommand
     }
 }
 
+def runMavenWithSettings(String settingsXmlId, String goals, boolean skipTests) {
+    Properties properties = new Properties()
+    properties.put('skipTests', skipTests)
+    runMavenWithSettings(settingsXmlId, goals, properties)
+}
+
 def runMavenWithSubmarineSettings(String goals, boolean skipTests) {
-    runMavenWithSettings('9239af2e-46e3-4ba3-8dd6-1a814fc8a56d', goals, skipTests)
+    runMavenWithSettings(SUBMARINE_SETTINGS, goals, skipTests)
+}
+
+def runMavenWithSubmarineSettings(String goals, Properties properties) {
+    runMavenWithSettings(SUBMARINE_SETTINGS, goals, properties)
 }
