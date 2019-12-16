1f2426dd6a4e16b25cc323f4fbf6d5bf254cefdb
Enrique Mingorance Cano <emingora@redhat.com>
2019-12-16 11:46:30 +0100

    BXMSPROD-592 treebuild script

---
+++ vars/treebuild.groovy
@@ -0,0 +1,40 @@
+def downstreamBuild(String treeFilePath) {
+    def lines = new File(treeFilePath).readLines()
+    def lastLine = lines.get(lines.size() - 1)
+
+    println "Downstream building ${lastLine} project"
+    upstreamBuild(treeFilePath, lastLine)
+}
+
+def upstreamBuild(String treeFilePath, String currentProject) {
+    println "Upstream building ${currentProject} project"
+
+    def projects = new File(treeFilePath).readLines()
+
+    // Build project tree from currentProject node
+    def currentProjectFound = false
+    for (i = projects.size() - 1; i >= 0; i--) {
+        def project = projects.get(i)
+        if(currentProjectFound) {
+            // build project
+            buildProject(project)
+        }
+        if(project == currentProject) {
+            currentProjectFound = true
+        }
+    }
+
+    buildProject(currentProject)
+}
+
+def buildProject(String project) {
+    def projectGroup = project.split("\\/")[0]
+    def projectName = project.split("\\/")[1]
+    println "Buiding ${projectGroup}/${projectName}"
+
+    sh "mkdir ${projectGroup}_${projectName}"
+    sh "cd ${projectGroup}_${projectName}"
+    githubscm.checkoutIfExists(projectName, "$CHANGE_AUTHOR", "$CHANGE_BRANCH", projectGroup, "$CHANGE_TARGET")
+    maven.runMavenWithSubmarineSettings('clean install', true)
+    sh ".."
+}
