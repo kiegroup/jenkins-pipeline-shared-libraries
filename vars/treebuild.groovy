5f32f1b2426c15a264bfb487f14ca708c4fa2604
Enrique Mingorance Cano <emingora@redhat.com>
2019-12-17 10:47:27 +0100

    BXMSPROD-592 `new File` removed from script

--- vars/treebuild.groovy
+++ vars/treebuild.groovy
@@ -1,24 +1,34 @@
-def downstreamBuild(String treeFilePath) {
-    def lines = new File(treeFilePath).readLines()
-    def lastLine = lines.get(lines.size() - 1)
+/**
+ * Builds the downstream
+ * @param projectCollection a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
+ */
+def downstreamBuild(def projectCollection) {
+    def lastLine = projectCollection.get(projectCollection.size() - 1)
 
     println "Downstream building ${lastLine} project"
-    upstreamBuild(treeFilePath, lastLine)
+    upstreamBuild(projectCollection, lastLine)
 }
 
-def upstreamBuild(String treeFilePath, String currentProject) {
+/**
+ * Builds the upstream for an specific project
+ * @param projectCollection a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
+ * @param currentProject the project to build the stream from, like kiegroup/drools
+ */
+def upstreamBuild(def projectCollection, String currentProject) {
     println "Upstream building ${currentProject} project"
 
-    def projects = new File(treeFilePath).readLines()
-
     // Build project tree from currentProject node
-    for (i = 0; currentProject != projects.get(i); i++) {
-        buildProject(projects.get(i))
+    for (i = 0; currentProject != projectCollection.get(i); i++) {
+        buildProject(projectCollection.get(i))
     }
 
     buildProject(currentProject)
 }
 
+/**
+ *
+ * @param project a string following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
+ */
 def buildProject(String project) {
     def projectGroup = project.split("\\/")[0]
     def projectName = project.split("\\/")[1]
@@ -28,9 +38,15 @@ def buildProject(String project) {
     sh "cd ${projectGroup}_${projectName}"
     githubscm.checkoutIfExists(projectName, "$CHANGE_AUTHOR", "$CHANGE_BRANCH", projectGroup, "$CHANGE_TARGET")
     maven.runMavenWithSubmarineSettings('clean install', true)
-    sh ".."
+    sh "cd .."
 }
 
+/**
+ *
+ * @param projectUrl the github project url
+ */
 def getProject(String projectUrl) {
     return (projectUrl =~ /((git|ssh|http(s)?)|(git@[\w\.]+))(:(\/\/)?(github.com\\/))([\w\.@\:\/\-~]+)(\.git)(\/)?/)[0][8]
 }
+
+return this;
