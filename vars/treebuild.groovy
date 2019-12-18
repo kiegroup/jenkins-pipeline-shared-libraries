e15e97fd102171c4cf45fc4cd74287c213ca8bdd
Enrique Mingorance Cano <emingora@redhat.com>
2019-12-18 14:59:02 +0100

    BXMSPROD-592 treebuild.groovy more generic (#4)
    * BXMSPROD-592 treebuild.groovy more generic
    
    * BXMSPROD-592 projectCollection replaced by the filepath
    
    * BXMSPROD-592 vars/tree.txt removed

--- vars/treebuild.groovy
+++ vars/treebuild.groovy
@@ -1,42 +1,56 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+
 /**
  * Builds the downstream
- * @param projectCollection a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
+ * @param filePath the path to a file with a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
+ * @param settingsXmlId maven settings xml file id
+ * @param goals maven goals
+ * @param skipTests boolean to skip tests or not
  */
-def downstreamBuild(def projectCollection) {
+def downstreamBuild(String filePath, String settingsXmlId, String goals, boolean skipTests) {
+    def projectCollection = Files.readAllLines(Paths.get(filePath))
     def lastLine = projectCollection.get(projectCollection.size() - 1)
 
     println "Downstream building ${lastLine} project"
-    upstreamBuild(projectCollection, lastLine)
+    upstreamBuild(filePath, lastLine, settingsXmlId, goals, skipTests)
 }
 
 /**
  * Builds the upstream for an specific project
- * @param projectCollection a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
+ * @param filePath the path to a file with a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
  * @param currentProject the project to build the stream from, like kiegroup/drools
+ * @param settingsXmlId maven settings xml file id
+ * @param goals maven goals
+ * @param skipTests boolean to skip tests or not
  */
-def upstreamBuild(def projectCollection, String currentProject) {
+def upstreamBuild(String filePath, String currentProject, String settingsXmlId, String goals, boolean skipTests) {
     println "Upstream building ${currentProject} project"
 
+    def projectCollection = Files.readAllLines(Paths.get(filePath))
     // Build project tree from currentProject node
     for (i = 0; currentProject != projectCollection.get(i); i++) {
-        buildProject(projectCollection.get(i))
+        buildProject(projectCollection.get(i), settingsXmlId, goals, skipTests)
     }
 
-    buildProject(currentProject)
+    buildProject(currentProject, settingsXmlId, goals, skipTests)
 }
 
 /**
  *
  * @param project a string following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
+ * @param settingsXmlId maven settings xml file id
+ * @param goals maven goals
+ * @param skipTests boolean to skip tests or not
  */
-def buildProject(String project) {
+def buildProject(String project, String settingsXmlId, String goals, boolean skipTests) {
     def projectGroup = project.split("\\/")[0]
     def projectName = project.split("\\/")[1]
     println "Building ${projectGroup}/${projectName}"
     sh "mkdir -p ${projectGroup}_${projectName}"
     sh "cd ${projectGroup}_${projectName}"
     githubscm.checkoutIfExists(projectName, "$CHANGE_AUTHOR", "$CHANGE_BRANCH", projectGroup, "$CHANGE_TARGET")
-    maven.runMavenWithSettings('771ff52a-a8b4-40e6-9b22-d54c7314aa1e', 'clean install', true)
+    maven.runMavenWithSettings(settingsXmlId, goals, skipTests)
     sh "cd .."
 }
 
