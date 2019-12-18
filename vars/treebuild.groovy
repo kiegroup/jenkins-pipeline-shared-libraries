b62c135112e5406c115d03f9b7e3390e30472417
Enrique Mingorance Cano <emingora@redhat.com>
2019-12-18 13:04:00 +0100

    BXMSPROD-592 projectCollection replaced by the filepath

--- vars/treebuild.groovy
+++ vars/treebuild.groovy
@@ -1,28 +1,33 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+
 /**
  * Builds the downstream
- * @param projectCollection a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
+ * @param filePath the path to a file with a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
  * @param settingsXmlId maven settings xml file id
  * @param goals maven goals
  * @param skipTests boolean to skip tests or not
  */
-def downstreamBuild(Collection<String> projectCollection, String settingsXmlId, String goals, boolean skipTests) {
+def downstreamBuild(String filePath, String settingsXmlId, String goals, boolean skipTests) {
+    def projectCollection = Files.readAllLines(Paths.get(filePath))
     def lastLine = projectCollection.get(projectCollection.size() - 1)
 
     println "Downstream building ${lastLine} project"
-    upstreamBuild(projectCollection, lastLine, settingsXmlId, goals, skipTests)
+    upstreamBuild(filePath, lastLine, settingsXmlId, goals, skipTests)
 }
 
 /**
  * Builds the upstream for an specific project
- * @param projectCollection a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
+ * @param filePath the path to a file with a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
  * @param currentProject the project to build the stream from, like kiegroup/drools
  * @param settingsXmlId maven settings xml file id
  * @param goals maven goals
  * @param skipTests boolean to skip tests or not
  */
-def upstreamBuild(Collection<String> projectCollection, String currentProject, String settingsXmlId, String goals, boolean skipTests) {
+def upstreamBuild(String filePath, String currentProject, String settingsXmlId, String goals, boolean skipTests) {
     println "Upstream building ${currentProject} project"
 
+    def projectCollection = Files.readAllLines(Paths.get(filePath))
     // Build project tree from currentProject node
     for (i = 0; currentProject != projectCollection.get(i); i++) {
         buildProject(projectCollection.get(i), settingsXmlId, goals, skipTests)
