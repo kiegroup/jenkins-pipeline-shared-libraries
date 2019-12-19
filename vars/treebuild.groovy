0c34b9d4a58ecd9eab40c582cc8c80257b4da58b
Enrique Mingorance Cano <emingora@redhat.com>
2019-12-19 09:06:23 +0100

    treebuild.groovy test

--- vars/treebuild.groovy
+++ vars/treebuild.groovy
@@ -3,37 +3,41 @@ import java.nio.file.Paths
 
 /**
  * Builds the downstream
- * @param filePath the path to a file with a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
+ * @param projectsFileId the Jenkins ID for the file with a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
  * @param settingsXmlId maven settings xml file id
  * @param goals maven goals
  * @param skipTests boolean to skip tests or not
  */
-def downstreamBuild(String filePath, String settingsXmlId, String goals, boolean skipTests) {
-    def projectCollection = Files.readAllLines(Paths.get(filePath))
-    def lastLine = projectCollection.get(projectCollection.size() - 1)
+def downstreamBuild(String projectsFileId, String settingsXmlId, String goals, boolean skipTests) {
+    configFileProvider([configFile(fileId: projectsFileId, variable: 'PROJECTS_FILE')]) {
+        def projectCollection = Files.readAllLines(Paths.get(PROJECTS_FILE))
+        def lastLine = projectCollection.get(projectCollection.size() - 1)
 
-    println "Downstream building ${lastLine} project"
-    upstreamBuild(filePath, lastLine, settingsXmlId, goals, skipTests)
+        println "Downstream building ${lastLine} project"
+        upstreamBuild(projectsFileId, lastLine, settingsXmlId, goals, skipTests)
+    }
 }
 
 /**
  * Builds the upstream for an specific project
- * @param filePath the path to a file with a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
+ * @param projectsFileId the Jenkins ID for the file with a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
  * @param currentProject the project to build the stream from, like kiegroup/drools
  * @param settingsXmlId maven settings xml file id
  * @param goals maven goals
  * @param skipTests boolean to skip tests or not
  */
-def upstreamBuild(String filePath, String currentProject, String settingsXmlId, String goals, boolean skipTests) {
-    println "Upstream building ${currentProject} project"
+def upstreamBuild(String projectsFileId, String currentProject, String settingsXmlId, String goals, boolean skipTests) {
+    configFileProvider([configFile(fileId: projectsFileId, variable: 'PROJECTS_FILE')]) {
+        println "Upstream building ${currentProject} project"
 
-    def projectCollection = Files.readAllLines(Paths.get(filePath))
-    // Build project tree from currentProject node
-    for (i = 0; currentProject != projectCollection.get(i); i++) {
-        buildProject(projectCollection.get(i), settingsXmlId, goals, skipTests)
+        def projectCollection = Files.readAllLines(Paths.get(PROJECTS_FILE))
+        // Build project tree from currentProject node
+        for (i = 0; currentProject != projectCollection.get(i); i++) {
+            buildProject(projectCollection.get(i), settingsXmlId, goals, skipTests)
+        }
+
+        buildProject(currentProject, settingsXmlId, goals, skipTests)
     }
-
-    buildProject(currentProject, settingsXmlId, goals, skipTests)
 }
 
 /**
