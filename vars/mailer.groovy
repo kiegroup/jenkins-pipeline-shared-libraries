b3a48682f1bf7254a5d3b1481f018278db93207a
Enrique Mingorance Cano <emingora@redhat.com>
2020-04-01 09:42:45 +0200

    BXMSPROD-730 mailer script improved to treat also PRs

--- vars/mailer.groovy
+++ vars/mailer.groovy
@@ -1,6 +1,7 @@
 def sendEmailFailure() {
+    def branch = $BRANCH_NAME ?: $ghprbSourceBranch
     emailext (
-            subject: "Build $BRANCH_NAME failed",
-            body: "Build $BRANCH_NAME failed! For more information see $BUILD_URL",
+            subject: "Build $branch failed",
+            body: "Build $branch failed! For more information see $BUILD_URL",
             recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']])
 }
