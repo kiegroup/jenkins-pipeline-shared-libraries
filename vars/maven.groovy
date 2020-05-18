e7682d8ad73fb1c73e3bef62d76da101603a6d32
Enrique Mingorance Cano <emingora@redhat.com>
2020-05-18 14:50:57 +0200

    Revert "BXMSPROD-753: adapted scripts for using PRs FDBS and CDBs (#23)" (#24)
    This reverts commit e4cd856c923a1239937921baf2600145df306ea4.

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -27,17 +27,3 @@ def runMavenWithSubmarineSettings(String goals, boolean skipTests, String logFil
 def runMavenWithSubmarineSettings(String goals, Properties properties, String logFileName = null) {
     runMavenWithSettings('9239af2e-46e3-4ba3-8dd6-1a814fc8a56d', goals, properties, logFileName)
 }
-/**
- *
- * @param settingsXmlId settings.xml file
- * @param goals maven gals
- * @param sonarCloudId Jenkins token for SonarCloud*
- */
-def runMavenWithSettingsSonar(String settingsXmlId, String goals, String sonarCloudId, String logFileName = null) {
-    configFileProvider([configFile(fileId: settingsXmlId, variable: 'MAVEN_SETTINGS_XML')]) {
-        withCredentials([string(credentialsId: sonarCloudId, variable: 'TOKEN')]) {
-            def teeCommand = logFileName ? '| tee $WORKSPACE/'+ logFileName + ' ; test ${PIPESTATUS[0]} -eq 0' : ''
-            sh "mvn -B -s $MAVEN_SETTINGS_XML -Dsonar.login=${TOKEN} ${goals} ${teeCommand}"
-        }
-    }
-}
