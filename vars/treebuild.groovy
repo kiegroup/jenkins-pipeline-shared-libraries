3613393d92b80da15325266c9f1068d6683dae9b
Enrique Mingorance Cano <emingora@redhat.com>
2020-06-05 12:14:56 +0200

    BXMSPROD-807 treebuild adapted to multiple goals per project

--- vars/treebuild.groovy
+++ vars/treebuild.groovy
@@ -1,15 +1,49 @@
 /**
  * Builds the downstream
- * @param the file with a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
+ * @param projectGoalsMap the map with project as key and different maven goals per project
+ * @param settingsXmlId maven settings xml file id
+ * @param skipTests Boolean to skip tests or not
+ */
+ def build(Map<String, List<String>> projectGoalsMap, String settingsXmlId, Boolean skipTests = null) {
+    println "Downstream building. Reading Lines for ${projectGoalsMap.keySet()}"
+    def lastLine = projectGoalsMap.keySet().collect().get(projectGoalsMap.keySet().collect().size() - 1)
+    println "Downstream building ${lastLine} project."
+    upstreamBuild(projectGoalsMap, lastLine, settingsXmlId, skipTests)
+}
+
+/**
+ * Builds the downstream
+ * @param projectCollection collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
  * @param settingsXmlId maven settings xml file id
  * @param goals maven goals
  * @param skipTests Boolean to skip tests or not
  */
 def build(List<String> projectCollection, String settingsXmlId, String goals, Boolean skipTests = null) {
-    println "Downstream building. Reading Lines for ${projectCollection}"
-    def lastLine = projectCollection.get(projectCollection.size() - 1)
-    println "Downstream building ${lastLine} project."
-    upstreamBuild(projectCollection, lastLine, settingsXmlId, goals, skipTests)
+    Map<String, List<String>> projectGoalsMap = [:].withDefault { [] }
+    projectCollection.each {
+        projectGoalsMap[it] << goals
+    }
+    build(projectGoalsMap, settingsXmlId, skipTests)
+}
+
+/**
+ * Builds the upstream for an specific project
+ * @param projectGoalsMap the map with project as key and different maven goals per project
+ * @param currentProject the project to build the stream from, like kiegroup/drools
+ * @param settingsXmlId maven settings xml file id
+ * @param skipTests Boolean to skip tests or not
+ */
+def upstreamBuild(Map<String, List<String>> projectGoalsMap, String currentProject, String settingsXmlId, Boolean skipTests = null) {
+    def projectCollection = projectGoalsMap.keySet().collect()
+    println "Upstream building ${currentProject} project for ${projectCollection}"
+    util.checkoutProjects(projectCollection, currentProject)
+
+    // Build project tree from currentProject node
+    for (i = 0; i == 0 || currentProject != projectCollection.get(i-1); i++) {
+        projectGoalsMap[projectCollection.get(i)].each {
+            util.buildProject(projectCollection.get(i), settingsXmlId, goals, skipTests)
+        }
+    }
 }
 
 /**
@@ -21,12 +55,9 @@ def build(List<String> projectCollection, String settingsXmlId, String goals, Bo
  * @param skipTests Boolean to skip tests or not
  */
 def upstreamBuild(List<String> projectCollection, String currentProject, String settingsXmlId, String goals, Boolean skipTests = null) {
-    println "Upstream building ${currentProject} project for ${projectCollection}"
-
-    util.checkoutProjects(projectCollection, currentProject)
-
-    // Build project tree from currentProject node
-    for (i = 0; i == 0 || currentProject != projectCollection.get(i-1); i++) {
-        util.buildProject(projectCollection.get(i), settingsXmlId, goals, skipTests)
+    Map<String, List<String>> projectGoalsMap = [:].withDefault { [] }
+    projectCollection.each {
+        projectGoalsMap[it] << goals
     }
+    upstreamBuild(projectGoalsMap, currentProject, settingsXmlId, skipTests)
 }
