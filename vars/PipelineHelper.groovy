4e9fd9d28271be4cde4116131ea19bbdcb3d1d22
ginxo <emingora@redhat.com>
2020-06-11 08:26:12 +0200

    BXMSPROD-815 retryAction implemented

--- vars/PipelineHelper.groovy
+++ vars/PipelineHelper.groovy
@@ -7,7 +7,7 @@ def class PipelineHelper {
         this.steps = steps
     }
 
-    void retry(Closure<?> action, int maxAttempts, int timeoutSeconds) {
+    void retry(Closure<?> action, int maxAttempts, int timeoutSeconds, Closure<?> errorAction = null) {
         steps.retry(count: maxAttempts) {
             steps.timeout(time: timeoutSeconds, unit: 'SECONDS') {
                 try {
@@ -15,6 +15,14 @@ def class PipelineHelper {
                 } catch (FlowInterruptedException e) {
                     steps.println '[ERROR] Timeout exceeded'
                     steps.error('Failing build because Timeout')
+                    if(errorAction) {
+                        try {
+                            errorAction.call();
+                        } catch(Exception errorActionException) {
+                            steps.println "[ERROR] Error executing error action ${errorActionException.getMessage()}"
+                            throw errorActionException
+                        }
+                    }
                 }
             }
         }
