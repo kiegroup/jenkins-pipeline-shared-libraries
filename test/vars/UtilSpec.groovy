d2a0df2666c37ea9074eacd9df4893d5fe59f53d
ginxo <emingora@redhat.com>
2020-06-17 07:55:02 +0200

    BXMSPROD-826 util.groovy covered by tests

---
+++ test/vars/UtilSpec.groovy
@@ -0,0 +1,120 @@
+import com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification
+import hudson.plugins.git.GitSCM
+
+class UtilSpec extends JenkinsPipelineSpecification {
+    def groovyScript = null
+
+    def setup() {
+        groovyScript = loadPipelineScriptForTest("vars/util.groovy")
+        explicitlyMockPipelineVariable("out")
+    }
+
+    def "[util.groovy] getProject"() {
+        when:
+        def project = groovyScript.getProject('https://github.com/kiegroup/jenkins-pipeline-shared-libraries.git')
+        then:
+        project == 'kiegroup/jenkins-pipeline-shared-libraries'
+    }
+
+    def "[util.groovy] getGroup"() {
+        when:
+        def group = groovyScript.getGroup('https://github.com/kiegroup/jenkins-pipeline-shared-libraries.git')
+        then:
+        group == 'kiegroup'
+    }
+
+    def "[util.groovy] getProjectGroupName with group"() {
+        when:
+        def groupName = groovyScript.getProjectGroupName('name', 'group')
+        then:
+        groupName[0] == 'group'
+        groupName[1] == 'name'
+    }
+
+    def "[util.groovy] getProjectGroupName without group"() {
+        when:
+        def groupName = groovyScript.getProjectGroupName('name')
+        then:
+        groupName[0] == 'kiegroup'
+        groupName[1] == 'name'
+    }
+
+    def "[util.groovy] getGoals with type"() {
+        setup:
+        groovyScript.getBinding().setVariable('PROPERTIES_FILE', 'propertiesFile.txt')
+        when:
+        def goals = groovyScript.getGoals('project1', 'fileId', 'typex')
+        then:
+        1 * getPipelineMock("configFile.call")(['fileId': 'fileId', 'variable': 'PROPERTIES_FILE']) >> { return 'configFile' }
+        1 * getPipelineMock("configFileProvider.call")(['configFile'], _ as Closure)
+        1 * getPipelineMock("readProperties")(['file': 'propertiesFile.txt']) >> {
+            return ['goals': ['project1': ['typex': 'typexValue', 'default': 'defaultValue']]]
+        }
+        // Pending to check goals value
+//         goals == "typexValue"
+    }
+
+    def "[util.groovy] isProjectTriggeringJob true"() {
+        setup:
+        def projectGroupName = ['group', 'name']
+        def env = [:]
+        env['ghprbGhRepository'] = 'group/name'
+        groovyScript.getBinding().setVariable("env", env)
+        when:
+        def result = groovyScript.isProjectTriggeringJob(projectGroupName)
+        then:
+        result
+    }
+
+    def "[util.groovy] isProjectTriggeringJob false different project"() {
+        setup:
+        def projectGroupName = ['group', 'name']
+        def env = [:]
+        env['ghprbGhRepository'] = 'group/name1'
+        groovyScript.getBinding().setVariable("env", env)
+        when:
+        def result = groovyScript.isProjectTriggeringJob(projectGroupName)
+        then:
+        !result
+    }
+
+    def "[util.groovy] storeGitInformation GIT_INFORMATION_REPORT null"() {
+        setup:
+        def projectGroupName = ['group', 'name']
+        def env = [:]
+        groovyScript.getBinding().setVariable("env", env)
+        when:
+        groovyScript.storeGitInformation('projectName')
+        then:
+        1 * getPipelineMock('githubscm.getCommit')() >> 'kiegroup/lienzo-core: 0f917d4 Expose zoom and pan filters (#102)'
+        1 * getPipelineMock('githubscm.getBranch')() >> '* (detached from 0f917d4)  remotes/origin/master'
+        1 * getPipelineMock('githubscm.getRemoteInfo')('origin', 'url') >> 'https://github.com/kiegroup/lienzo-core.git'
+        env['GIT_INFORMATION_REPORT'] == "projectName=kiegroup/lienzo-core: 0f917d4 Expose zoom and pan filters (#102) Branch [* (detached from 0f917d4)  remotes/origin/master] Remote [https://github.com/kiegroup/lienzo-core.git]"
+    }
+
+    def "[util.groovy] storeGitInformation GIT_INFORMATION_REPORT previous value"() {
+        setup:
+        def projectGroupName = ['group', 'name']
+        def env = [:]
+        env['GIT_INFORMATION_REPORT'] = 'projectName=kiegroup/lienzo-tests: 45c16e1 Fix tests (#84) Branch [* (detached from 45c16e1)  remotes/origin/master] Remote [https://github.com/kiegroup/lienzo-tests.git]'
+        groovyScript.getBinding().setVariable("env", env)
+        when:
+        groovyScript.storeGitInformation('projectName')
+        then:
+        1 * getPipelineMock('githubscm.getCommit')() >> 'kiegroup/lienzo-core: 0f917d4 Expose zoom and pan filters (#102)'
+        1 * getPipelineMock('githubscm.getBranch')() >> '* (detached from 0f917d4)  remotes/origin/master'
+        1 * getPipelineMock('githubscm.getRemoteInfo')('origin', 'url') >> 'https://github.com/kiegroup/lienzo-core.git'
+        env['GIT_INFORMATION_REPORT'] == 'projectName=kiegroup/lienzo-tests: 45c16e1 Fix tests (#84) Branch [* (detached from 45c16e1)  remotes/origin/master] Remote [https://github.com/kiegroup/lienzo-tests.git]; projectName=kiegroup/lienzo-core: 0f917d4 Expose zoom and pan filters (#102) Branch [* (detached from 0f917d4)  remotes/origin/master] Remote [https://github.com/kiegroup/lienzo-core.git]'
+    }
+
+    def "[util.groovy] printGitInformationReport GIT_INFORMATION_REPORT null"() {
+        setup:
+        def projectGroupName = ['group', 'name']
+        def env = [:]
+        groovyScript.getBinding().setVariable("env", env)
+        when:
+        groovyScript.printGitInformationReport()
+        then:
+        true
+    }
+}
