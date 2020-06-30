48db7c8f4521da004c66c291f6f26ca74311565b
ginxo <emingora@redhat.com>
2020-06-30 12:15:25 +0200

    PLANNER-2044 Jenkinsfile.nightly additionalVariablesBranched7 mechanism test

---
+++ test/resources/nightlyMethods.groovy
@@ -0,0 +1,9 @@
+def additionalVariablesBranched7(def branched7RepositoryListPath = "./script/branched-7-repository-list.txt") {
+    def currentBranch = env.BRANCH_NAME ?: env.GIT_BRANCH
+    def additionalVariables = [:]
+    if ('master' == currentBranch) {
+        def branched7RepositoryListFile = readFile branched7RepositoryListPath
+        branched7RepositoryListFile.readLines().each { it -> additionalVariables["${it}-scmRevision"] = '7.x' }
+    }
+    return additionalVariables
+}
