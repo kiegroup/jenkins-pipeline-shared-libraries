7b7af2e84438e885efd18a72cc1f481a8116a468
Enrique Mingorance Cano <emingora@redhat.com>
2020-12-03 16:59:05 +0100

    pipelinehelper better exception handling (#123)
    * pipelinehelper better exception handling
    
    * test to cover the new case
    
    * FlowInterruptedException always treated
    
    * Update vars/PipelineHelper.groovy
    
    Co-authored-by: Tristan Radisson <tristan.radisson@gmail.com>
    
    Co-authored-by: Tristan Radisson <tristan.radisson@gmail.com>

--- test/vars/PipelineHelperSpec.groovy
+++ test/vars/PipelineHelperSpec.groovy
@@ -49,6 +49,38 @@ class PipelineHelperSpec extends JenkinsPipelineSpecification {
         notThrown(FlowInterruptedException)
     }
 
+    def "[PipelineHelper.groovy] different exception"() {
+        setup:
+        def pipelineHelper = new PipelineHelper(steps)
+        when:
+        pipelineHelper.retry({
+            sh 'ls'
+            throw new MissingPropertyException("exception")
+        }, 1, 1,  null, MissingPropertyException.class)
+        then:
+        1 * getPipelineMock("sh")('ls')
+        notThrown(MissingPropertyException)
+    }
+
+    def "[PipelineHelper.groovy] different exception retry timeout"() {
+        setup:
+        def pipelineHelper = new PipelineHelper(steps)
+
+        def caseInterruption = new CauseOfInterruption() {
+            String getShortDescription() {
+                return "description"
+            }
+        }
+        when:
+        pipelineHelper.retry({
+            sh 'ls'
+            throw new FlowInterruptedException(new Result('result', BallColor.RED, 0, false), caseInterruption)
+        }, 1, 1, null, MissingPropertyException.class)
+        then:
+        1 * getPipelineMock("sh")('ls')
+        notThrown(FlowInterruptedException)
+    }
+
     def "[PipelineHelper.groovy] exception"() {
         setup:
         def pipelineHelper = new PipelineHelper(steps)
