18bfaf1cb96a80488f0792d87116ab52895061b4
ginxo <emingora@redhat.com>
2020-12-03 10:33:48 +0100

    pipelinehelper better exception handling

--- vars/PipelineHelper.groovy
+++ vars/PipelineHelper.groovy
@@ -1,3 +1,4 @@
+import groovy.transform.PackageScope
 import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
 
 def class PipelineHelper {
@@ -7,23 +8,27 @@ def class PipelineHelper {
         this.steps = steps
     }
 
-    void retry(Closure<?> action, int maxAttempts, int timeoutSeconds, Closure<?> errorAction = null) {
+    public <T extends Exception> void retry(Closure<?> action, int maxAttempts, int timeoutSeconds, Closure<?> errorAction = null, Class<T> exceptionType = FlowInterruptedException) {
         int counter = 0
         steps.retry(count: maxAttempts) {
             steps.timeout(time: timeoutSeconds, unit: 'SECONDS') {
                 try {
                     steps.println "[INFO] Executing ${counter + 1}/${maxAttempts}"
-                    action.call();
-                } catch (FlowInterruptedException e) {
-                    steps.println "[ERROR] Timeout exceeded in ${counter + 1}/${maxAttempts}"
-                    steps.error("Failing build because Timeout ${counter + 1}/${maxAttempts}")
-                    if(errorAction) {
-                        try {
-                            errorAction.call();
-                        } catch(Exception errorActionException) {
-                            steps.println "[ERROR] Error executing error action ${errorActionException.getMessage()}"
-                            throw errorActionException
+                    action.call()
+                } catch (Exception e) {
+                    if (exceptionType.isInstance(e)) {
+                        steps.println "[ERROR] Timeout exceeded in ${counter + 1}/${maxAttempts}"
+                        steps.error("Failing build because Timeout ${counter + 1}/${maxAttempts}")
+                        if (errorAction) {
+                            try {
+                                errorAction.call()
+                            } catch (Exception errorActionException) {
+                                steps.println "[ERROR] Error executing error action ${errorActionException.getMessage()}"
+                                throw errorActionException
+                            }
                         }
+                    } else {
+                        throw e
                     }
                 } finally {
                     counter++
