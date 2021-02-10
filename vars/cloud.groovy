979b3f81c36b709d16b6d8d3a8b09bb1e10039ed
Tristan Radisson <tristan.radisson@gmail.com>
2021-02-10 11:41:02 +0100

    Updated cloud methods to accept 2 types of creds (#139)

--- vars/cloud.groovy
+++ vars/cloud.groovy
@@ -1,9 +1,9 @@
 /*
 * Make a quay.io image public if not already
-*/ 
-void makeQuayImagePublic(String namespace, String repository, String quayTokenCredentialsId) {
-    if(!isQuayImagePublic(namespace, repository, quayTokenCredentialsId)
-        && !setQuayImagePublic(namespace, repository, quayTokenCredentialsId)){
+*/
+void makeQuayImagePublic(String namespace, String repository, Map credentials = [ 'token': '', 'usernamePassword': '' ]) {
+    if (!isQuayImagePublic(namespace, repository, credentials)
+        && !setQuayImagePublic(namespace, repository, credentials)) {
         error "Cannot set image quay.io/${namespace}/${repository} as visible"
     }
 }
@@ -11,9 +11,9 @@ void makeQuayImagePublic(String namespace, String repository, String quayTokenCr
 /*
 * Checks whether a quay image is public
 */
-boolean isQuayImagePublic(String namespace, String repository, String quayTokenCredentialsId) {
+boolean isQuayImagePublic(String namespace, String repository, Map credentials = [ 'token': '', 'usernamePassword': '' ]) {
     def output = 'false'
-    withCredentials([string(credentialsId: quayTokenCredentialsId, variable: 'QUAY_TOKEN')]) {
+    util.executeWithCredentialsMap(credentials) {
         output = sh(returnStdout: true, script: "curl -H 'Authorization: Bearer ${QUAY_TOKEN}' -X GET https://quay.io/api/v1/repository/${namespace}/${repository} | jq '.is_public'").trim()
     }
     return output == 'true'
@@ -24,9 +24,9 @@ boolean isQuayImagePublic(String namespace, String repository, String quayTokenC
 *
 * return false if any problem occurs
 */
-boolean setQuayImagePublic(String namespace, String repository, String quayTokenCredentialsId) {
+boolean setQuayImagePublic(String namespace, String repository, Map credentials = [ 'token': '', 'usernamePassword': '' ]) {
     def output = 'false'
-    withCredentials([string(credentialsId: quayTokenCredentialsId, variable: 'QUAY_TOKEN')]) {
+    util.executeWithCredentialsMap(credentials) {
         output = sh(returnStdout: true, script: "curl -H 'Content-Type: application/json' -H 'Authorization: Bearer ${QUAY_TOKEN}' -X POST --data '{\"visibility\": \"public\"}' https://quay.io/api/v1/repository/${namespace}/${repository}/changevisibility | jq '.success'").trim()
     }
     return output == 'true'
