7f4d04c30095a78c9050c45a0a90aeac501cf6d1
ginxo <emingora@redhat.com>
2021-04-27 11:20:52 +0200

    [BXMSPROD-1289] getLatestArtifactFromRepository

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,36 +1,36 @@
 import java.util.Properties
 import org.kie.jenkins.MavenCommand
 
-def runMaven(String goals, List options=[], Properties properties=null, String logFileName = null) {
+def runMaven(String goals, List options = [], Properties properties = null, String logFileName = null) {
     new MavenCommand(this)
-        .withOptions(options)
-        .withProperties(properties)
-        .withLogFileName(logFileName)
-        .run(goals)
+            .withOptions(options)
+            .withProperties(properties)
+            .withLogFileName(logFileName)
+            .run(goals)
 }
 
-def runMaven(String goals, boolean skipTests, List options=[], String logFileName = null) {
+def runMaven(String goals, boolean skipTests, List options = [], String logFileName = null) {
     new MavenCommand(this)
-        .withOptions(options)
-        .skipTests(skipTests)
-        .withLogFileName(logFileName)
-        .run(goals)
+            .withOptions(options)
+            .skipTests(skipTests)
+            .withLogFileName(logFileName)
+            .run(goals)
 }
 
 def runMavenWithSettings(String settingsXmlId, String goals, Properties properties, String logFileName = null) {
     new MavenCommand(this, ['-fae'])
-        .withSettingsXmlId(settingsXmlId)
-        .withProperties(properties)
-        .withLogFileName(logFileName)
-        .run(goals)
+            .withSettingsXmlId(settingsXmlId)
+            .withProperties(properties)
+            .withLogFileName(logFileName)
+            .run(goals)
 }
 
 def runMavenWithSettings(String settingsXmlId, String goals, boolean skipTests, String logFileName = null) {
     new MavenCommand(this, ['-fae'])
-        .withSettingsXmlId(settingsXmlId)
-        .skipTests(skipTests)
-        .withLogFileName(logFileName)
-        .run(goals)
+            .withSettingsXmlId(settingsXmlId)
+            .skipTests(skipTests)
+            .withLogFileName(logFileName)
+            .run(goals)
 }
 
 def runMavenWithSubmarineSettings(String goals, boolean skipTests, String logFileName = null) {
@@ -41,7 +41,7 @@ def runMavenWithSubmarineSettings(String goals, Properties properties, String lo
     runMavenWithSettings(getSubmarineSettingsXmlId(), goals, properties, logFileName)
 }
 
-String getSubmarineSettingsXmlId(){
+String getSubmarineSettingsXmlId() {
     return '9239af2e-46e3-4ba3-8dd6-1a814fc8a56d'
 }
 
@@ -54,49 +54,52 @@ String getSubmarineSettingsXmlId(){
 def runMavenWithSettingsSonar(String settingsXmlId, String goals, String sonarCloudId, String logFileName = null) {
     withCredentials([string(credentialsId: sonarCloudId, variable: 'TOKEN')]) {
         new MavenCommand(this)
-            .withSettingsXmlId(settingsXmlId)
-            .withProperty('sonar.login', "${TOKEN}")
-            .withLogFileName(logFileName)
-            .run(goals)
+                .withSettingsXmlId(settingsXmlId)
+                .withProperty('sonar.login', "${TOKEN}")
+                .withLogFileName(logFileName)
+                .run(goals)
     }
 }
 
 def mvnVersionsSet(String newVersion, boolean allowSnapshots = false) {
     mvnVersionsSet(new MavenCommand(this), newVersion, allowSnapshots)
 }
+
 def mvnVersionsSet(MavenCommand mvnCmd, String newVersion, boolean allowSnapshots = false) {
     mvnCmd.clone()
-        .withOptions(['-N', '-e'])
-        .withProperty('full')
-        .withProperty('newVersion', newVersion)
-        .withProperty('allowSnapshots', allowSnapshots)
-        .withProperty('generateBackupPoms', false)
-        .run('versions:set')
+            .withOptions(['-N', '-e'])
+            .withProperty('full')
+            .withProperty('newVersion', newVersion)
+            .withProperty('allowSnapshots', allowSnapshots)
+            .withProperty('generateBackupPoms', false)
+            .run('versions:set')
 }
 
 def mvnVersionsUpdateParent(String newVersion, boolean allowSnapshots = false) {
     mvnVersionsUpdateParent(new MavenCommand(this), newVersion, allowSnapshots)
 }
+
 def mvnVersionsUpdateParent(MavenCommand mvnCmd, String newVersion, boolean allowSnapshots = false) {
     mvnCmd.clone()
-        .withOptions(['-N', '-e'])
-        .withProperty('full')
-        .withProperty('parentVersion', "[${newVersion}]")
-        .withProperty('allowSnapshots', allowSnapshots)
-        .withProperty('generateBackupPoms', false)
-        .run('versions:update-parent')
+            .withOptions(['-N', '-e'])
+            .withProperty('full')
+            .withProperty('parentVersion', "[${newVersion}]")
+            .withProperty('allowSnapshots', allowSnapshots)
+            .withProperty('generateBackupPoms', false)
+            .run('versions:update-parent')
 }
 
 def mvnVersionsUpdateChildModules(boolean allowSnapshots = false) {
     mvnVersionsUpdateChildModules(new MavenCommand(this), allowSnapshots)
 }
+
 def mvnVersionsUpdateChildModules(MavenCommand mvnCmd, boolean allowSnapshots = false) {
     mvnCmd.clone()
-        .withOptions(['-N', '-e'])
-        .withProperty('full')
-        .withProperty('allowSnapshots', allowSnapshots)
-        .withProperty('generateBackupPoms', false)
-        .run('versions:update-child-modules')
+            .withOptions(['-N', '-e'])
+            .withProperty('full')
+            .withProperty('allowSnapshots', allowSnapshots)
+            .withProperty('generateBackupPoms', false)
+            .run('versions:update-child-modules')
 }
 
 def mvnVersionsUpdateParentAndChildModules(String newVersion, boolean allowSnapshots = false) {
@@ -114,12 +117,12 @@ def mvnSetVersionProperty(String property, String newVersion) {
 
 def mvnSetVersionProperty(MavenCommand mvnCmd, String property, String newVersion) {
     mvnCmd.clone()
-        .withOptions(['-e'])
-        .withProperty('property', property)
-        .withProperty('newVersion', newVersion)
-        .withProperty('allowSnapshots', true)
-        .withProperty('generateBackupPoms', false)
-        .run('versions:set-property')
+            .withOptions(['-e'])
+            .withProperty('property', property)
+            .withProperty('newVersion', newVersion)
+            .withProperty('allowSnapshots', true)
+            .withProperty('generateBackupPoms', false)
+            .run('versions:set-property')
 }
 
 def uploadLocalArtifacts(String mvnUploadCredsId, String artifactDir, String repoUrl) {
@@ -132,9 +135,16 @@ def uploadLocalArtifacts(String mvnUploadCredsId, String artifactDir, String rep
     }
 }
 
+def getLatestArtifactFromRepository(String repositoryUrl, String groupId, String artifactId) {
+    def groupIdArtifactId = "${groupId.replaceAll("\\.", "/")}/${artifactId}"
+    def metadata = new XmlSlurper().parse("${repositoryUrl}/${groupIdArtifactId}/maven-metadata.xml")
+    return metadata.versioning && metadata.versioning.latest ? metadata.versioning.latest.text() : null
+}
+
 /*
 * Clean Maven repository on the node
 */
+
 void cleanRepository() {
     sh 'rm -rf $HOME/.m2/repository'
 }
