098f083d9eae99f720df691b595d59a7840f76b5
Enrique Mingorance Cano <emingora@redhat.com>
2021-04-28 21:36:23 +0200

    [BXMSPROD-1289] getLatestArtifactFromRepository (#150)
    * [BXMSPROD-1289] getLatestArtifactFromRepository
    
    * getLatestArtifactVersionPrefixFromRepository
    
    * tristan suggestion applied
    
    * getLatestArtifactVersionFromRepository suggestion applied
    
    * getLatestArtifactVersionPrefixFromRepository null test
    
    * Update vars/maven.groovy
    
    Co-authored-by: Tristan Radisson <tristan.radisson@gmail.com>
    
    * getMavenMetadata extracted
    
    Co-authored-by: Tristan Radisson <tristan.radisson@gmail.com>

--- test/vars/MavenSpec.groovy
+++ test/vars/MavenSpec.groovy
@@ -1,7 +1,22 @@
 import com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification
+import groovy.util.slurpersupport.GPathResult
+import groovy.util.slurpersupport.NodeChild
+import groovy.util.slurpersupport.NodeChildren
 
 class MavenSpec extends JenkinsPipelineSpecification {
     def mavenGroovy = null
+
+    class VersionChildNode {
+        String version
+
+        VersionChildNode(String version) {
+            this.version = version;
+        }
+
+        def text() {
+            return this.version;
+        }
+    }
 
     def setup() {
         mavenGroovy = loadPipelineScriptForTest("vars/maven.groovy")
@@ -57,7 +72,7 @@ class MavenSpec extends JenkinsPipelineSpecification {
 
     def "[maven.groovy] run Maven with settings with log file"() {
         setup:
-        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML':'settingsFileId']) 
+        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML': 'settingsFileId'])
         Properties properties = new Properties()
         properties.put('property1', 'value1')
         when:
@@ -68,7 +83,7 @@ class MavenSpec extends JenkinsPipelineSpecification {
 
     def "[maven.groovy] run Maven with settings without log file"() {
         setup:
-        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML':'settingsFileId']) 
+        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML': 'settingsFileId'])
         Properties properties = new Properties()
         properties.put('property1b', 'value1b')
         when:
@@ -79,7 +94,7 @@ class MavenSpec extends JenkinsPipelineSpecification {
 
     def "[maven.groovy] run Maven with settings without properties"() {
         setup:
-        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML':'settingsFileId']) 
+        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML': 'settingsFileId'])
         Properties properties = new Properties()
         when:
         mavenGroovy.runMavenWithSettings("settings.xml", "clean install", properties, "logFile.txt")
@@ -89,8 +104,8 @@ class MavenSpec extends JenkinsPipelineSpecification {
 
     def "[maven.groovy] run Maven sonar settings with log file"() {
         setup:
-        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML':'settingsFileId']) 
-        mavenGroovy.getBinding().setVariable("TOKEN", 'tokenId') 
+        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML': 'settingsFileId'])
+        mavenGroovy.getBinding().setVariable("TOKEN", 'tokenId')
         when:
         mavenGroovy.runMavenWithSettingsSonar("settings.xml", "clean install", "sonarCloudId", "logFile.txt")
         then:
@@ -99,8 +114,8 @@ class MavenSpec extends JenkinsPipelineSpecification {
 
     def "[maven.groovy] run Maven sonar settings without log file"() {
         setup:
-        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML':'settingsFileId']) 
-        mavenGroovy.getBinding().setVariable("TOKEN", 'tokenId') 
+        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML': 'settingsFileId'])
+        mavenGroovy.getBinding().setVariable("TOKEN", 'tokenId')
         when:
         mavenGroovy.runMavenWithSettingsSonar("settings.xml", "clean install", "sonarCloudId")
         then:
@@ -109,7 +124,7 @@ class MavenSpec extends JenkinsPipelineSpecification {
 
     def "[maven.groovy] run with Settings with log file"() {
         setup:
-        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML':'settingsFileId']) 
+        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML': 'settingsFileId'])
         Properties properties = new Properties()
         properties.put('skipTests', true)
         when:
@@ -118,9 +133,9 @@ class MavenSpec extends JenkinsPipelineSpecification {
         1 * getPipelineMock("sh")([script: 'mvn -B -s settingsFileId -fae clean install -DskipTests=true | tee $WORKSPACE/logFile.txt ; test ${PIPESTATUS[0]} -eq 0', returnStdout: false])
     }
 
-     def "[maven.groovy] run with Settings without log file"() {
+    def "[maven.groovy] run with Settings without log file"() {
         setup:
-        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML':'settingsFileId']) 
+        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML': 'settingsFileId'])
         when:
         mavenGroovy.runMavenWithSettings("settings.xml", "clean install", false)
         then:
@@ -129,7 +144,7 @@ class MavenSpec extends JenkinsPipelineSpecification {
 
     def "[maven.groovy] run with Submarine Settings without properties and without log file"() {
         setup:
-        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML':'settingsFileId']) 
+        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML': 'settingsFileId'])
         when:
         mavenGroovy.runMavenWithSubmarineSettings("clean install", false)
         then:
@@ -138,7 +153,7 @@ class MavenSpec extends JenkinsPipelineSpecification {
 
     def "[maven.groovy] run with Submarine Settings with properties and without log file"() {
         setup:
-        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML':'settingsFileId']) 
+        mavenGroovy.getBinding().setVariable("env", ['MAVEN_SETTINGS_XML': 'settingsFileId'])
         Properties properties = new Properties()
         when:
         mavenGroovy.runMavenWithSubmarineSettings("clean install", properties)
@@ -148,105 +163,177 @@ class MavenSpec extends JenkinsPipelineSpecification {
 
     def "[maven.groovy] run mvn versions set"() {
         setup:
-            def String newVersion = '3.6.2'
+        def String newVersion = '3.6.2'
         when:
-            mavenGroovy.mvnVersionsSet(newVersion) 
+        mavenGroovy.mvnVersionsSet(newVersion)
         then:
-            1 * getPipelineMock("sh")([script: "mvn -B -N -e versions:set -Dfull -DnewVersion=${newVersion} -DallowSnapshots=false -DgenerateBackupPoms=false", returnStdout: false])
+        1 * getPipelineMock("sh")([script: "mvn -B -N -e versions:set -Dfull -DnewVersion=${newVersion} -DallowSnapshots=false -DgenerateBackupPoms=false", returnStdout: false])
     }
 
     def "[maven.groovy] run mvn versions set with allow snapshots"() {
         setup:
-            def String newVersion = '3.6.2'
+        def String newVersion = '3.6.2'
         when:
-            mavenGroovy.mvnVersionsSet(newVersion, true) 
+        mavenGroovy.mvnVersionsSet(newVersion, true)
         then:
-            1 * getPipelineMock("sh")([script: "mvn -B -N -e versions:set -Dfull -DnewVersion=${newVersion} -DallowSnapshots=true -DgenerateBackupPoms=false", returnStdout: false])
+        1 * getPipelineMock("sh")([script: "mvn -B -N -e versions:set -Dfull -DnewVersion=${newVersion} -DallowSnapshots=true -DgenerateBackupPoms=false", returnStdout: false])
     }
 
     def "[maven.groovy] run mvn versions update parent"() {
         setup:
-            def String newVersion = '3.6.2'
+        def String newVersion = '3.6.2'
         when:
-            mavenGroovy.mvnVersionsUpdateParent(newVersion) 
+        mavenGroovy.mvnVersionsUpdateParent(newVersion)
         then:
-            1 * getPipelineMock("sh")([script: "mvn -B -N -e versions:update-parent -Dfull -DparentVersion=[${newVersion}] -DallowSnapshots=false -DgenerateBackupPoms=false", returnStdout: false])
+        1 * getPipelineMock("sh")([script: "mvn -B -N -e versions:update-parent -Dfull -DparentVersion=[${newVersion}] -DallowSnapshots=false -DgenerateBackupPoms=false", returnStdout: false])
     }
 
     def "[maven.groovy] run mvn versions update parent with allow snapshots"() {
         setup:
-            def String newVersion = '3.6.2'
+        def String newVersion = '3.6.2'
         when:
-            mavenGroovy.mvnVersionsUpdateParent(newVersion, true) 
+        mavenGroovy.mvnVersionsUpdateParent(newVersion, true)
         then:
-            1 * getPipelineMock("sh")([script: "mvn -B -N -e versions:update-parent -Dfull -DparentVersion=[${newVersion}] -DallowSnapshots=true -DgenerateBackupPoms=false", returnStdout: false])
+        1 * getPipelineMock("sh")([script: "mvn -B -N -e versions:update-parent -Dfull -DparentVersion=[${newVersion}] -DallowSnapshots=true -DgenerateBackupPoms=false", returnStdout: false])
     }
 
     def "[maven.groovy] run mvn versions update child modules"() {
         when:
-            mavenGroovy.mvnVersionsUpdateChildModules() 
+        mavenGroovy.mvnVersionsUpdateChildModules()
         then:
-            1 * getPipelineMock("sh")([script: 'mvn -B -N -e versions:update-child-modules -Dfull -DallowSnapshots=false -DgenerateBackupPoms=false', returnStdout: false])
+        1 * getPipelineMock("sh")([script: 'mvn -B -N -e versions:update-child-modules -Dfull -DallowSnapshots=false -DgenerateBackupPoms=false', returnStdout: false])
     }
 
     def "[maven.groovy] run mvn versions update child modules with allow snapshots"() {
         when:
-            mavenGroovy.mvnVersionsUpdateChildModules(true) 
+        mavenGroovy.mvnVersionsUpdateChildModules(true)
         then:
-            1 * getPipelineMock("sh")([script: 'mvn -B -N -e versions:update-child-modules -Dfull -DallowSnapshots=true -DgenerateBackupPoms=false', returnStdout: false])
+        1 * getPipelineMock("sh")([script: 'mvn -B -N -e versions:update-child-modules -Dfull -DallowSnapshots=true -DgenerateBackupPoms=false', returnStdout: false])
     }
 
-   def "[maven.groovy] run mvn versions update parent and child modules"() {
+    def "[maven.groovy] run mvn versions update parent and child modules"() {
         setup:
-            def String newVersion = '3.6.2'
+        def String newVersion = '3.6.2'
         when:
-            mavenGroovy.mvnVersionsUpdateParentAndChildModules(newVersion) 
+        mavenGroovy.mvnVersionsUpdateParentAndChildModules(newVersion)
         then:
-            1 * getPipelineMock("sh")([script: "mvn -B -N -e versions:update-parent -Dfull -DparentVersion=[${newVersion}] -DallowSnapshots=false -DgenerateBackupPoms=false", returnStdout: false])
-            1 * getPipelineMock("sh")([script: 'mvn -B -N -e versions:update-child-modules -Dfull -DallowSnapshots=false -DgenerateBackupPoms=false', returnStdout: false])
+        1 * getPipelineMock("sh")([script: "mvn -B -N -e versions:update-parent -Dfull -DparentVersion=[${newVersion}] -DallowSnapshots=false -DgenerateBackupPoms=false", returnStdout: false])
+        1 * getPipelineMock("sh")([script: 'mvn -B -N -e versions:update-child-modules -Dfull -DallowSnapshots=false -DgenerateBackupPoms=false', returnStdout: false])
     }
 
     def "[maven.groovy] run mvn versions update parent and child modules with allow snapshots"() {
         setup:
-            def String newVersion = '3.6.2'
+        def String newVersion = '3.6.2'
         when:
-            mavenGroovy.mvnVersionsUpdateParentAndChildModules(newVersion, true) 
+        mavenGroovy.mvnVersionsUpdateParentAndChildModules(newVersion, true)
         then:
-            1 * getPipelineMock("sh")([script: "mvn -B -N -e versions:update-parent -Dfull -DparentVersion=[${newVersion}] -DallowSnapshots=true -DgenerateBackupPoms=false", returnStdout: false])
-            1 * getPipelineMock("sh")([script: 'mvn -B -N -e versions:update-child-modules -Dfull -DallowSnapshots=true -DgenerateBackupPoms=false', returnStdout: false])
+        1 * getPipelineMock("sh")([script: "mvn -B -N -e versions:update-parent -Dfull -DparentVersion=[${newVersion}] -DallowSnapshots=true -DgenerateBackupPoms=false", returnStdout: false])
+        1 * getPipelineMock("sh")([script: 'mvn -B -N -e versions:update-child-modules -Dfull -DallowSnapshots=true -DgenerateBackupPoms=false', returnStdout: false])
     }
 
     def "[maven.groovy] run mvn set version property"() {
         setup:
-            String newVersion = '1.2.3'
-            String propertyName = 'version.org.kie.kogito'
+        String newVersion = '1.2.3'
+        String propertyName = 'version.org.kie.kogito'
         when:
-            mavenGroovy.mvnSetVersionProperty(propertyName, newVersion)
+        mavenGroovy.mvnSetVersionProperty(propertyName, newVersion)
         then:
         1 * getPipelineMock("sh")([script: "mvn -B -e versions:set-property -Dproperty=$propertyName -DnewVersion=$newVersion -DallowSnapshots=true -DgenerateBackupPoms=false", returnStdout: false])
     }
 
     def "[maven.groovy] uploadLocalArtifacts"() {
         setup:
-            String mvnUploadCredsId = 'mvnUploadCredsId'
-            String mvnUploadCreds = 'user:password'
-            mavenGroovy.getBinding().setVariable('kieUnpack', mvnUploadCreds)
-            String artifactDir = '/tmp'
-            String repoUrl = 'https://redhat.com'
+        String mvnUploadCredsId = 'mvnUploadCredsId'
+        String mvnUploadCreds = 'user:password'
+        mavenGroovy.getBinding().setVariable('kieUnpack', mvnUploadCreds)
+        String artifactDir = '/tmp'
+        String repoUrl = 'https://redhat.com'
         when:
-            mavenGroovy.uploadLocalArtifacts(mvnUploadCredsId, artifactDir, repoUrl)
+        mavenGroovy.uploadLocalArtifacts(mvnUploadCredsId, artifactDir, repoUrl)
         then:
-            1 * getPipelineMock('usernameColonPassword.call')([credentialsId: mvnUploadCredsId, variable: 'kieUnpack']) >> mvnUploadCreds
-            1 * getPipelineMock('withCredentials')([mvnUploadCreds], _ as Closure)
-            1 * getPipelineMock('dir')(artifactDir, _ as Closure)
-            1 * getPipelineMock('sh')('zip -r artifacts .')
-            1 * getPipelineMock('sh')("curl --silent --upload-file artifacts.zip -u ${mvnUploadCreds} -v ${repoUrl}")
+        1 * getPipelineMock('usernameColonPassword.call')([credentialsId: mvnUploadCredsId, variable: 'kieUnpack']) >> mvnUploadCreds
+        1 * getPipelineMock('withCredentials')([mvnUploadCreds], _ as Closure)
+        1 * getPipelineMock('dir')(artifactDir, _ as Closure)
+        1 * getPipelineMock('sh')('zip -r artifacts .')
+        1 * getPipelineMock('sh')("curl --silent --upload-file artifacts.zip -u ${mvnUploadCreds} -v ${repoUrl}")
+    }
+
+    def "[maven.groovy] getLatestArtifactVersionFromRepository OK"() {
+        setup:
+        String expectedVersion = '7.11.0.redhat-210426'
+        String repositoryUrl = 'http://repoUrl'
+        String groupId = 'org.kie.rhba'
+        String artifactId = 'rhdm'
+        def xmlSlurper = GroovySpy(XmlSlurper.class, global: true)
+        def gPathResult = Mock(GPathResult.class)
+        when:
+        def result = mavenGroovy.getLatestArtifactVersionFromRepository(repositoryUrl, groupId, artifactId)
+        then:
+        1 * xmlSlurper.parse('http://repoUrl/org/kie/rhba/rhdm/maven-metadata.xml') >> gPathResult
+        1 * gPathResult.getProperty('versioning') >> gPathResult
+        1 * gPathResult.getProperty('latest') >> gPathResult
+        1 * gPathResult.text() >> expectedVersion
+        expectedVersion == result
+    }
+
+    def "[maven.groovy] getLatestArtifactVersionFromRepository null"() {
+        setup:
+        String expectedVersion = null
+        String repositoryUrl = 'http://repoUrl'
+        String groupId = 'org.kie.rhba'
+        String artifactId = 'rhdm'
+        def xmlSlurper = GroovySpy(XmlSlurper.class, global: true)
+        def gPathResult = Mock(GPathResult.class)
+        when:
+        def result = mavenGroovy.getLatestArtifactVersionFromRepository(repositoryUrl, groupId, artifactId)
+        then:
+        1 * xmlSlurper.parse('http://repoUrl/org/kie/rhba/rhdm/maven-metadata.xml') >> gPathResult
+        1 * gPathResult.getProperty('versioning') >> null
+        expectedVersion == result
+    }
+
+    def "[maven.groovy] getLatestArtifactVersionPrefixFromRepository OK"() {
+        setup:
+        String expectedVersion = '7.52.0.Final-redhat-00004'
+        String repositoryUrl = 'http://repoUrl'
+        String groupId = 'org.kie'
+        String artifactId = 'kie-api'
+        def xmlSlurper = GroovySpy(XmlSlurper.class, global: true)
+        def gPathResult = Mock(GPathResult.class)
+        def versionIterator = [new VersionChildNode('7.52.0.Final'), new VersionChildNode('7.52.0.Final-redhat-00001'), new VersionChildNode('7.52.0.Final-redhat-00004'), new VersionChildNode('7.52.0.Final-redhat-00003'), new VersionChildNode('7.53.0.Final-redhat-00009')].iterator()
+        when:
+        def result = mavenGroovy.getLatestArtifactVersionPrefixFromRepository(repositoryUrl, groupId, artifactId, '7.52.0.Final-redhat')
+        then:
+        1 * xmlSlurper.parse('http://repoUrl/org/kie/kie-api/maven-metadata.xml') >> gPathResult
+        1 * gPathResult.getProperty('versioning') >> gPathResult
+        1 * gPathResult.getProperty('versions') >> gPathResult
+        1 * gPathResult.childNodes() >> versionIterator
+        expectedVersion == result
+    }
+
+    def "[maven.groovy] getLatestArtifactVersionPrefixFromRepository null"() {
+        setup:
+        String expectedVersion = null
+        String repositoryUrl = 'http://repoUrl'
+        String groupId = 'org.kie'
+        String artifactId = 'kie-api'
+        def xmlSlurper = GroovySpy(XmlSlurper.class, global: true)
+        def gPathResult = Mock(GPathResult.class)
+        def versionIterator = [new VersionChildNode('7.52.0.Final'), new VersionChildNode('7.52.0.Final-redhat-00001'), new VersionChildNode('7.52.0.Final-redhat-00004'), new VersionChildNode('7.52.0.Final-redhat-00003'), new VersionChildNode('7.53.0.Final-redhat-00009')].iterator()
+        when:
+        def result = mavenGroovy.getLatestArtifactVersionPrefixFromRepository(repositoryUrl, groupId, artifactId, '8.0.0.Final-redhat')
+        then:
+        1 * xmlSlurper.parse('http://repoUrl/org/kie/kie-api/maven-metadata.xml') >> gPathResult
+        1 * gPathResult.getProperty('versioning') >> gPathResult
+        1 * gPathResult.getProperty('versions') >> gPathResult
+        1 * gPathResult.childNodes() >> versionIterator
+        expectedVersion == result
     }
 
     def "[maven.groovy] cleanRepository"() {
         when:
-            mavenGroovy.cleanRepository()
+        mavenGroovy.cleanRepository()
         then:
-            1 * getPipelineMock('sh')('rm -rf $HOME/.m2/repository')
+        1 * getPipelineMock('sh')('rm -rf $HOME/.m2/repository')
     }
 }
