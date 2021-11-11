283e0aa03a845b18b638e87b816725284c3e7418
ginxo <emingora@redhat.com>
2021-11-11 13:20:40 +0100

    BXMSPROD-1543 to align composite-actions with the ones from kogito-pipelines

--- vars/buildChain.groovy
+++ vars/buildChain.groovy
@@ -8,13 +8,7 @@ def getBuildChainVersionFromCompositeActionFile(String actionFilePath = '.ci/act
     def actionFileContent = readFile actionFilePath
     def actionObject = new Yaml().load(actionFileContent)
 
-    def uses = actionObject.runs.steps.uses
-    def action = uses != null ? uses.find({ it.contains(usesContainingString) }) : null
-    if (action == null) {
-        throw new RuntimeException("There's not steps with 'uses' for build-chain ${usesContainingString}")
-    }
-
-    def buildChainScmRevision = action.substring(action.indexOf('@') + 1)
+    def buildChainScmRevision = actionObject.inputs['scm-revision'].default
     try {
         def url = new URL("https://raw.githubusercontent.com/kiegroup/github-action-build-chain/${buildChainScmRevision}/package.json")
         def packageJson = url.getText()
