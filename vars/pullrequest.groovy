de259281100777c091eff638d20269ae4ff5cbfe
jstastny-cz <jan.stastny@ibm.com>
2023-11-08 12:15:48 +0100

    fix repository variable

--- vars/pullrequest.groovy
+++ vars/pullrequest.groovy
@@ -62,6 +62,7 @@ void postComment(String commentText, String githubTokenCredsId = "kie-ci3-token"
         error "Pull Request Id variable (ghprbPullId or CHANGE_ID) is not set. Are you sure you are running with Github Branch Source plugin or ghprb plugin?"
     }
     def changeId = CHANGE_ID ?: ghprbPullId
+    def changeRepository = CHANGE_REPO ?: ghprbGhRepository
     String filename = "${util.generateHash(10)}.build.summary"
     def jsonComment = [
         body : commentText
@@ -69,7 +70,7 @@ void postComment(String commentText, String githubTokenCredsId = "kie-ci3-token"
     writeJSON(json: jsonComment, file: filename)
     sh "cat ${filename}"
     withCredentials([string(credentialsId: githubTokenCredsId, variable: 'GITHUB_TOKEN')]) {
-        sh "curl -s -H \"Authorization: token ${GITHUB_TOKEN}\" -X POST -d '@${filename}' \"https://api.github.com/repos/${BUILDCHAIN_PROJECT}/issues/${changeId}/comments\""
+        sh "curl -s -H \"Authorization: token ${GITHUB_TOKEN}\" -X POST -d '@${filename}' \"https://api.github.com/repos/${changeRepository}/issues/${changeId}/comments\""
     }
     sh "rm ${filename}"
 }
